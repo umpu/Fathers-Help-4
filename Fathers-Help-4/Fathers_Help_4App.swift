e2e92274137f9c0b49b5cc8e646be84af79698c3
umpu <carol.ledgers0h@icloud.com>
2024-03-10 17:07:17 +0500

    Initial Commit

---
+++ Fathers-Help-4/Fathers_Help_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Fathers_Help_4App.swift
+//  Fathers-Help-4
+//
+//  Created by Andrei Gorbunov on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Fathers_Help_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
