bbabca7447b173c865c07a70fae049d8d2108ba5
umpu <carol.ledgers0h@icloud.com>
2024-03-10 17:20:21 +0500

    Copying code base from last step

--- Fathers-Help-4/ContentView.swift
+++ Fathers-Help-4/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var activeAnimation = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button {
+            withAnimation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0.0)) {
+                activeAnimation = true
+            } completion: {
+                activeAnimation = false
+            }
+        } label: {
+            GeometryReader { proxy in
+                let width = proxy.size.width / 2
+                let systemName = "play.fill"
+                
+                HStack(alignment: .center, spacing: 0) {
+                    Image(systemName: systemName)
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: activeAnimation ? width : .zero)
+                        .opacity(activeAnimation ? 1 : .zero)
+                    
+                    Image(systemName: systemName)
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: width)
+                    
+                    Image(systemName: systemName)
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: activeAnimation ? 0.5 : width)
+                        .opacity(activeAnimation ? .zero : 1)
+                }
+                .frame(maxHeight: .infinity, alignment: .center)
+            }
         }
-        .padding()
+        .frame(maxWidth: 62)
     }
 }
 
