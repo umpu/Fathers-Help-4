0c6b1d00ee332f9ff83c4182388415c974df2082
umpu <carol.ledgers0h@icloud.com>
2024-03-27 10:46:12 +0500

    Perfect

--- Fathers-Help-4/ContentView.swift
+++ Fathers-Help-4/ContentView.swift
@@ -8,73 +8,75 @@
 import SwiftUI
 
 struct ButtonStyleView: ButtonStyle {
-    @State private var activeAnimation = false
-    let duration = 0.22
+    @State var performAnimation = false
+    let duration: TimeInterval = 0.22
+    let scale: CGFloat = 0.86
     
     func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .foregroundStyle(.blue)
-            .padding()
-            .background {
-                Circle()
-                    .opacity(activeAnimation ? 0.1 : 0)
+        ZStack {
+            Circle()
+                .foregroundStyle(.gray)
+                .opacity(performAnimation ? 0.3 : 0)
+                .frame(maxWidth: 100, maxHeight: 100)
+            configuration.label
+                .padding(12)
+        }
+        .scaleEffect(performAnimation ? scale : 1)
+        .animation(.easeOut(duration: duration), value: configuration.isPressed)
+        .onChange(of: configuration.isPressed) { _, newValue in
+            guard newValue else { return }
+            
+            withAnimation(
+                .easeOut(duration: duration)
+            ) {
+                performAnimation = true
+            } completion: {
+                performAnimation = false
             }
-            .scaleEffect(activeAnimation ? 0.86 : 1)
-            .animation(.easeOut(duration: duration), value: activeAnimation)
-            .onChange(of: configuration.isPressed) { _, newValue in
-                guard newValue else { return }
-                
-                withAnimation(
-                    .easeOut(duration: duration)
-                ) {
-                    activeAnimation = true
-                } completion: {
-                    activeAnimation = false
-                }
-            }
+        }
+    }
+}
+
+struct ImageView: View {
+    var body: some View {
+        Image(systemName: "play.fill")
+            .renderingMode(.template)
+            .resizable()
+            .scaledToFit()
     }
 }
 
 struct ContentView: View {
-    @State private var activeAnimation = false
+    @State var performAnimation: Bool = false
     
     var body: some View {
         Button {
-            withAnimation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0.0)) {
-                activeAnimation = true
-            } completion: {
-                activeAnimation = false
+            if !performAnimation {
+                withAnimation(.spring) {
+                    performAnimation = true
+                } completion: {
+                    performAnimation = false
+                }
             }
         } label: {
             GeometryReader { proxy in
                 let width = proxy.size.width / 2
-                let systemName = "play.fill"
                 
                 HStack(alignment: .center, spacing: 0) {
-                    Image(systemName: systemName)
-                        .renderingMode(.template)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: activeAnimation ? width : .zero)
-                        .opacity(activeAnimation ? 1 : .zero)
+                    ImageView()
+                        .frame(width: performAnimation ? width : .zero)
+                        .opacity(performAnimation ? 1 : .zero)
                     
-                    Image(systemName: systemName)
-                        .renderingMode(.template)
-                        .resizable()
-                        .scaledToFit()
+                    ImageView()
                         .frame(width: width)
                     
-                    Image(systemName: systemName)
-                        .renderingMode(.template)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: activeAnimation ? 0.5 : width)
-                        .opacity(activeAnimation ? .zero : 1)
+                    ImageView()
+                        .frame(width: performAnimation ? 0.5 : width)
+                        .opacity(performAnimation ? .zero : 1)
                 }
-                .frame(maxHeight: .infinity, alignment: .center)
             }
+            .frame(maxWidth: 62, maxHeight: 62)
         }
-        .frame(maxWidth: 100)
         .buttonStyle(ButtonStyleView())
     }
 }
